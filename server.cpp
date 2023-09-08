a505e753924c9b514ca7ce4eafd3c975e7f71111
Almaz Shinbay <shinbay.almaz@gmail.com>
2023-09-08 16:22:16 +0600

    Add protocol parsing

--- server.cpp
+++ server.cpp
@@ -5,9 +5,12 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <assert.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
+
+const int32_t k_max_msg = 4096;
 
 static void msg(const char *message) {
 		fprintf(stderr, "%s\n", message);
@@ -59,6 +62,46 @@ static int32_t write_all(int fd, const char *buf, size_t n) {
 				buf += rv;
 		}
 		return 0;
+}
+
+static int32_t one_request(int connfd) {
+		char rbuf[4 + k_max_msg + 1];
+		errno = 0;
+		int32_t err = read_full(connfd, rbuf, 4);
+		if (err) {
+				if (errno == 0) {
+						msg("EOF");
+				} else {
+						msg("read() error");
+				}
+				return err;
+		}
+
+		uint32_t len = 0;
+		memcpy(&len, rbuf, 4); // assume little endian
+		if (len > k_max_msg) {
+				msg("too long");
+				return -1;
+		}
+		
+		// request body
+		err = read_full(connfd, &rbuf[4], len);
+		if (err) {
+				msg("read() error");
+				return err;
+		}
+
+		// do something
+		rbuf[4 + len] = '\0';
+		printf("client says: %s\n", &rbuf[4]);
+		
+		// reply using same protocol
+		const char reply[] = "world";
+		char wbuf[4 + sizeof(reply)];
+		len = (uint32_t)strlen(reply);
+		memcpy(wbuf, &len, 4);
+		memcpy(&wbuf[4], reply, len);
+		return write_all(connfd, wbuf, 4 + len);
 }
 
 int main() {
