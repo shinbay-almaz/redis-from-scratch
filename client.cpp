a2ad5a9485a637d5a2214d400304488390fad73f
Almaz Shinbay <shinbay.almaz@gmail.com>
2023-09-11 17:10:20 +0600

    Add basic server commands: get, set, and del

--- client.cpp
+++ client.cpp
@@ -5,6 +5,8 @@
 #include <errno.h>
 #include <unistd.h>
 #include <assert.h>
+#include <string>
+#include <vector>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
@@ -49,16 +51,27 @@ static int32_t write_all(int fd, const char *buf, size_t n) {
 		return 0;
 }
 
-static int32_t send_req(int fd, const char *text) {
-		uint32_t len = (uint32_t)strlen(text);
+static int32_t send_req(int fd, const std::vector<std::string> &cmd) {
+		uint32_t len = 4;
+		for (const std::string &s : cmd) {
+				len += 4 + s.size();
+		}
 
 		if (len > k_max_msg) {
 				return -1;
 		}
 
 		char wbuf[4 + k_max_msg];
+		uint32_t n = cmd.size();
 		memcpy(&wbuf[0], &len, 4);
-		memcpy(&wbuf[4], text, len);
+		memcpy(&wbuf[4], &n, 4);
+		size_t pos = 8;
+		for (const std::string &s : cmd) {
+				uint32_t p = (uint32_t)s.size();
+				memcpy(&wbuf[pos], &p, 4);
+				memcpy(&wbuf[pos + 4], s.data(), s.size());
+				pos += 4 + p;
+		}
 
 		return write_all(fd, wbuf, 4 + len);
 }
@@ -95,11 +108,18 @@ static int32_t read_res(int fd) {
 		}
 
 		rbuf[4 + len] = '\0';
-		printf("server says: %s\n", &rbuf[4]);
+
+		uint32_t rescode = 0;
+		if (len < 4) {
+				msg("bad response");
+				return -1;
+		}
+		memcpy(&rescode, &rbuf[4], 4);
+		printf("server says: [%u] %.*s\n", rescode, len - 4, &rbuf[8]);
 		return 0;
 }
 
-int main() {
+int main(int argc, char **argv) {
 		int fd = socket(AF_INET, SOCK_STREAM, 0);
 
 		struct sockaddr_in addr = {};
@@ -112,19 +132,18 @@ int main() {
 				die("connect()");
 		}
 		
-		const char *query_list[3] = {"hello1", "hello2", "hello3"};
-		for (size_t i = 0; i < 3; i++) {
-				int32_t err = send_req(fd, query_list[i]);
-				if (err) {
-						goto L_DONE;
-				}
+		std::vector<std::string> cmd;
+		for (int i = 1; i < argc; i++) {
+				cmd.push_back(argv[i]);
 		}
 
-		for (size_t i = 0; i < 3; i++) {
-				int32_t err = read_res(fd);
-				if (err) {
-						goto L_DONE;
-				}
+		int32_t err = send_req(fd, cmd);
+		if (err) {
+				goto L_DONE;
+		}
+		err = read_res(fd);
+		if (err) {
+				goto L_DONE;
 		}
 
 	L_DONE:
